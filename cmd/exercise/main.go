2f911b1fe8e3b8cb6d700afc228aa062ba4b0327
David Collier-Brown <dave.collier-brown@indexexchange.com>
2023-02-04 21:01:58 -0500

    First testable implementation

--- cmd/exercise/main.go
+++ cmd/exercise/main.go
@@ -1,37 +1,131 @@
 package main
 
-// Exercise -- a program to exercise the simplest/stupidest possible cache for a
-//	particular task.
+// Exercise -- a program to exercise the simplest/stupidest possible stupidestCache.
+
+import (
+	"encoding/csv"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/davecb/stupidestCache/src/stupidestCache"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var eShort = errors.New("csv record was too short")
+var eBlank = errors.New("csv record was blank")
 
 // main -- get options and commands
 func main() {
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Fprint(os.Stderr, "You must supply a load.csv file\n") //nolint
+		usage()
+	}
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime) // show file:line in logs
+	filename := flag.Arg(0)
+	if filename == "" {
+		log.Fatalf("No load-test .csv file provided, halting.\n")
+	}
+	exercise(filename)
 
 }
 
-func exercise() {
-	var key, value string
-	var c = cache.New()
+func exercise(filename string) {
+	var record []string
+	var operation, key, value string
+	var err error
+	var cache = stupidestCache.New()
+	defer cache.Close()
 
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error opening %s: %s, halting.", filename, err)
+	}
+	defer f.Close() // nolint
+
+	r := csv.NewReader(f)
+	r.Comma = ' '
+	r.Comment = '#'
+	r.FieldsPerRecord = -1 // ignore differences
+
+loop:
 	for {
-		line, eof := xxx.Read()
-		if eof {
-			break
+		record, err = r.Read()
+		switch {
+		case err == io.EOF:
+			break loop
+		case err != nil:
+			log.Fatalf("failure in csv.Read(), halting. err = %v, record = %q\n", err, record)
 		}
-		switch operation {
-		case "r":
-			x, present = c.Get(key)
-			if x != operand {
-				error
-			}
-		case "w":
-			err = c.Put(key, value)
-			if err != nil {
-				error
-			}
-		default:
-			//illformed 			line
-		}
+		log.Printf("exercise read %q\n", record)
 
+		operation, key, value, err = parseCsv(record)
+		if err == eBlank {
+			// ignore blank csv lines, we don't mind
+			continue
+		}
+		if err != nil {
+			log.Fatalf("parse error, halting. err = %v, record = %q\n", err, record)
+		}
+		log.Printf("operation = %q, key = %q, value = %q, err = %v\n", operation, key, value, err)
+
+		switch operation {
+		case "g":
+			x, present := cache.Get(key)
+			interpretGet(present, x, key, value)
+
+		case "p":
+			err = cache.Put(key, value)
+			interpretPut(err, key, value)
+
+		default:
+			log.Fatalf(`ill-formed csv line. Need either "p" or "g". record = %q`+"\n", record)
+		}
 	}
 	// all done
+}
+
+// interpretPut looks to see if we did a put operation correctly
+func interpretPut(err error, key string, value string) {
+	if err != nil {
+		log.Printf("couldn't put key = %q, value = %q, err = %v\n", key, value, err)
+	} else {
+		log.Printf("put suceeded\n")
+	}
+}
+
+// interpretGet looks to see if we could get an expected value from the cache
+func interpretGet(present bool, x, key, value string) {
+	if !present {
+		log.Printf("value not present, ill-formed experiment\n")
+	}
+	if x != value {
+		log.Printf("comparison failed. key = %q, stupidestCache = %q, input = %q\n", key, x, value)
+	} else {
+		log.Printf("get and comparison suceeded\n")
+	}
+}
+
+func usage() {
+	//nolint
+	fmt.Fprint(os.Stderr, "Usage: exercise file.csv") //nolint
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+// parseCsv will parse three or more field csv files for this experiment
+func parseCsv(record []string) (string, string, string, error) {
+	switch len(record) {
+	case 0:
+		return "", "", "", eBlank // technically an error, albeit harmless
+	case 1:
+		return record[0], "", "", eShort
+	case 2:
+		return record[0], record[1], "", eShort
+	default:
+		return record[0], record[1], strings.Join(record[2:], " "), nil
+	}
 }
--- src/stupidestCache/stupidestCache.go
+++ src/stupidestCache/stupidestCache.go
@@ -1,12 +1,84 @@
 package stupidestCache
 
-// Stupidest Cache -- a program to povide the simplest possible cache for a
-//	particular task,  to serve as a performance target
+// Stupidest Cache -- a program to provide the simplest possible cache for a
+//	particular task, to serve as a performance target
 
-// New
+type sCache struct {
+	m      map[string]string // the cache itself, not locked
+	ask    chan string       // a channel for asking bout keys
+	answer chan ve           // a channel for returning values
+	update chan kv           // a channel for updating keys & values
+	done   chan bool         // and a channel for shutting down
+}
+type kv struct {
+	k string
+	v string
+}
 
-// Get
+type ve struct {
+	value  string
+	exists bool
+}
 
-// Put
+// New creates a new stupidest cache
+func New() *sCache {
+	var s sCache
 
-// Close
+	if s.m != nil {
+		panic("you can't New() me twice, Dave! Die")
+	}
+	s.m = make(map[string]string)
+	s.ask = make(chan string)
+	s.answer = make(chan ve)
+	s.update = make(chan kv)
+	s.done = make(chan bool)
+	go s.stupid()
+	return &s
+}
+
+// Get takes a key and returns a value and an exists flag
+func (s *sCache) Get(key string) (string, bool) {
+	var ans ve
+
+	s.ask <- key
+	ans = <-s.answer
+	return ans.value, ans.exists
+}
+
+// Put takes a key and value and updates a cache line
+func (s *sCache) Put(key, value string) error {
+	var up kv
+
+	up.k = key
+	up.v = value
+	s.update <- up
+	return nil
+}
+
+// Close closes the cache
+func (s *sCache) Close() {
+	close(s.done) // tell the goroutine to exit
+	s.m = nil     // smash the map, to force freeing
+}
+
+// stupid -- the implementation
+func (s *sCache) stupid() {
+
+	for {
+		select {
+		case k := <-s.ask:
+			var v ve
+			// when given a key, reply with a value and an exists falg
+			val, exists := s.m[k]
+			v.value = val
+			v.exists = exists
+			s.answer <- v
+		case kv := <-s.update:
+			// when given a key and value, replace both
+			s.m[kv.k] = kv.v
+		case <-s.done:
+			// when given a done indication, quit
+			return
+		}
+	}
+}
